56e11d6fa72728bfaef42f6efe3d84b096525dc9
Tom Jowett <tom@tomjowett.com>
2015-03-23 23:29:13 +1100

    0.0.1

---
+++ Classes/AUBProduct.h
@@ -0,0 +1,13 @@
+//
+//  AUBProduct.h
+//  
+//
+//  Created by Tom Jowett on 23/03/2015.
+//
+//
+
+#import <Mantle/Mantle.h>
+
+@interface AUBProduct : MTLModel
+
+@end
---
+++ Classes/AUBProduct.m
@@ -0,0 +1,13 @@
+//
+//  AUBProduct.m
+//  
+//
+//  Created by Tom Jowett on 23/03/2015.
+//
+//
+
+#import "AUBProduct.h"
+
+@implementation AUBProduct
+
+@end
